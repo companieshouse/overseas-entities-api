7ad56e68ac2d192e881be682df5c675db6ff273c
markpit <mpitten@companieshouse.gov.uk>
2022-03-21 18:20:45 +0000

    ROE-55 Create an initial skeleton api

---
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/OverseasEntitiesApiApplication.java
@@ -0,0 +1,22 @@
+package uk.gov.companieshouse.overseasentitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class OverseasEntitiesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverseasEntitiesApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // This is to prevent times being out of time by an hour during British Summer Time in MongoDB
+        // MongoDB stores UTC datetime, and LocalDate doesn't contain timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+}
