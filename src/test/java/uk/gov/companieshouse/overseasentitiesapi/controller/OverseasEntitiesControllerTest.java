034ab8627671d6ba90367151cdd9a92ff1e41e4c
markpit <mpitten@companieshouse.gov.uk>
2022-03-22 12:23:59 +0000

    ROE-55 Add unit test for skeleton app

---
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/controller/OverseasEntitiesControllerTest.java
@@ -0,0 +1,20 @@
+package uk.gov.companieshouse.overseasentitiesapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OverseasEntitiesControllerTest {
+
+    private final OverseasEntitiesController overseasEntitiesController = new OverseasEntitiesController();
+
+    @Test
+    void testCreateNewSubmission() {
+        ResponseEntity<String> response = overseasEntitiesController.createNewSubmission();
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+        assertEquals("This is the Register an Overseas Entity API", response.getBody());
+    }
+}
