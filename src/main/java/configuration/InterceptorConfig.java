12484bc7c38931301c139754be21bfd1dac95d42
Matthew Culpin <mculpin@companieshouse.gov.uk>
2022-04-05 15:42:34 +0100

    ROE-169 scan path not correct

--- src/main/java/configuration/InterceptorConfig.java
+++ src/main/java/configuration/InterceptorConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan("uk.gov.companieshouse.api.interceptor")
+@ComponentScan("interceptor")
 public class InterceptorConfig implements WebMvcConfigurer {
 
     @Autowired
