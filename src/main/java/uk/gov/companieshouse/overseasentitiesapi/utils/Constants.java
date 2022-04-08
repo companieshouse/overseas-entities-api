52ce33c769b9cc560395a2f978efa09bb5e0584f
Matthew Culpin <mculpin@companieshouse.gov.uk>
2022-04-08 16:26:14 +0100

    ROE-206 constant in the wrong place

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/Constants.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/Constants.java
@@ -10,15 +10,14 @@ public class Constants {
 
     // URI path attributes
     public static final String TRANSACTION_ID_KEY = "transaction_id";
+    // Request attribute names
+    public static final String TRANSACTION_KEY = "transaction";
 
     // URIs
     public static final String SUBMISSION_URI_PATTERN = "/transactions/%s/overseas-entity/%s";
     public static final String VALIDATION_STATUS_URI_SUFFIX = "/validation-status";
     public static final String TRANSACTIONS_PRIVATE_API_PREFIX = "/private/transactions/";
     public static final String TRANSACTIONS_PUBLIC_API_PREFIX = "/transactions/";
-
-    // Request attribute names
-    public static final String TRANSACTION_KEY = "transaction";
 
     public static final String OVERSEAS_ENTITY_ID_KEY = "overseas_entity_id";
     // Filings
