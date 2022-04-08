03343ab193f6eea7ae166fff64b88c47e974ab7b
mwestacott <mwestacott2@companieshouse.gov.uk>
2022-04-08 15:04:00 +0100

    ROE-207 Added missing annotations and request id to log output

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/interceptor/FilingInterceptor.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/interceptor/FilingInterceptor.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.overseasentitiesapi.interceptor;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
@@ -20,11 +21,11 @@ import static uk.gov.companieshouse.api.model.transaction.TransactionStatus.CLOS
 public class FilingInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        ApiLogger.debug("Called preHandle(...)");
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
+        final String reqId = request.getHeader(ERIC_REQUEST_ID_KEY);
+        ApiLogger.debugContext(reqId, "Called preHandle(...)", null);
 
         final var transaction = (Transaction) request.getAttribute(TRANSACTION_KEY);
-        final String reqId = request.getHeader(ERIC_REQUEST_ID_KEY);
 
         if (Objects.isNull(transaction)) {
             ApiLogger.infoContext(reqId, "No transaction found in request - filing disallowed");
