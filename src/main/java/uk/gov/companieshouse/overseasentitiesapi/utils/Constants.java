7aab5b5684a3619d6456159cbd7ecef7e94587c7
markpit <mpitten@companieshouse.gov.uk>
2022-05-12 11:23:30 +0100

    ROE-552 Add costs link to the transaction resource

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/Constants.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/Constants.java
@@ -21,6 +21,7 @@ public class Constants {
     // URIs
     public static final String SUBMISSION_URI_PATTERN = "/transactions/%s/overseas-entity/%s";
     public static final String VALIDATION_STATUS_URI_SUFFIX = "/validation-status";
+    public static final String COSTS_URI_SUFFIX = "/costs";
     public static final String TRANSACTIONS_PRIVATE_API_PREFIX = "/private/transactions/";
     public static final String TRANSACTIONS_PUBLIC_API_PREFIX = "/transactions/";
 
