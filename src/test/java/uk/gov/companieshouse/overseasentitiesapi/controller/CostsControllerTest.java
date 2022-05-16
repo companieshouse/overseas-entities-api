9b2613107eabd3a6a368e1368ac86f62efa86072
markpit <mpitten@companieshouse.gov.uk>
2022-05-16 19:19:44 +0100

    ROE-78 Update CostsControllerTest

--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/controller/CostsControllerTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/controller/CostsControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.overseasentitiesapi.service.CostsService;
 
 import static org.mockito.Mockito.times;
@@ -12,6 +13,9 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class CostsControllerTest {
+    private final static String REQUEST_ID = "7567675jgjhgh";
+
+    private final Transaction transaction = new Transaction();
 
     @Mock
     private CostsService costService;
@@ -21,8 +25,8 @@ class CostsControllerTest {
 
     @Test
     void getCosts() {
-//        var response = costsController.getCosts();
-//
-//        verify(costService, times(1)).getCosts();
+        var response = costsController.getCosts(transaction, REQUEST_ID);
+
+        verify(costService, times(1)).getCosts();
     }
 }
