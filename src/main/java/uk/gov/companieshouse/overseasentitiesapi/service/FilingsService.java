efda9b5e9da186af92198184e874a0a3f673b865
jphillips2121 <jphillips3@companieshouse.gov.uk>
2022-07-05 11:40:03 +0100

    Remove unused import

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/service/FilingsService.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/service/FilingsService.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.filinggenerator.FilingApi;
