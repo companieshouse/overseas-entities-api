eb09314691bbf0ce0d5b9f41461fec9a53f14835
markpit <mpitten@companieshouse.gov.uk>
2022-08-24 17:49:48 +0100

    ROE-1197 Update Entity Name validation

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/validation/EntityDtoValidator.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/validation/EntityDtoValidator.java
@@ -3,15 +3,22 @@ package uk.gov.companieshouse.overseasentitiesapi.validation;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.overseasentitiesapi.model.dto.EntityDto;
 import uk.gov.companieshouse.overseasentitiesapi.model.dto.OverseasEntitySubmissionDto;
-import uk.gov.companieshouse.overseasentitiesapi.validation.utils.UtilsValidators;
+import uk.gov.companieshouse.overseasentitiesapi.validation.utils.StringValidators;
 import uk.gov.companieshouse.service.rest.err.Errors;
 
 @Component
 public class EntityDtoValidator {
 
     public Errors validate(EntityDto entityDto, Errors errs, String loggingContext) {
-        UtilsValidators.validateNotNull(entityDto.getName(), getEntityFieldName(EntityDto.NAME_FIELD), errs, loggingContext);
+        validateName(entityDto.getName(), errs, loggingContext);
         return errs;
+    }
+
+    private boolean validateName(String entityName, Errors errs, String loggingContext) {
+        String location = getEntityFieldName(EntityDto.NAME_FIELD);
+        return StringValidators.validateStringNotBlank(entityName, location, errs, loggingContext)
+                && StringValidators.validateLength(entityName, 160, location, errs, loggingContext)
+                && StringValidators.validateCharacters(entityName, location, errs, loggingContext);
     }
 
     private String getEntityFieldName(String fieldName) {
