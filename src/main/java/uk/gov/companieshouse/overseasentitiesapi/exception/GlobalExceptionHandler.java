de3817dcf72d4619db8997ada648acfb986236d7
markpit <mpitten@companieshouse.gov.uk>
2022-10-25 16:19:18 +0100

    ROE-1447 WIP

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/exception/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/exception/GlobalExceptionHandler.java
@@ -1,22 +1,48 @@
 package uk.gov.companieshouse.overseasentitiesapi.exception;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.owasp.encoder.Encode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import uk.gov.companieshouse.overseasentitiesapi.utils.ApiLogger;
 
 import java.util.HashMap;
+import java.util.Objects;
+
+import static uk.gov.companieshouse.overseasentitiesapi.utils.Constants.ERIC_REQUEST_ID_KEY;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Value("${GLOBAL_EXCEPTION_HANDLER_TRUNCATE_LENGTH_CHARS:15000}")
+    private int truncationLength;
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex) {
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest webRequest) {
+        var context = webRequest.getHeader(ERIC_REQUEST_ID_KEY);
+        var exceptionMessage = truncate(Encode.forJava(ex.getMessage()));
+        var encodedStackTrace = truncate(Encode.forJava(ExceptionUtils.getStackTrace(ex)));
+        var encodedRootCause = truncate(Encode.forJava(ExceptionUtils.getStackTrace(ExceptionUtils.getRootCause(ex))));
+
         HashMap<String, Object> logMap = new HashMap<>();
-        logMap.put("message", ex.getMessage());
         logMap.put("error", ex.getClass());
-        ApiLogger.error(ex.getMessage(), ex, logMap);
+        logMap.put("stackTrace", encodedStackTrace);
+        logMap.put("rootCause", encodedRootCause);
+
+        ApiLogger.errorContext(context, exceptionMessage, null, logMap);
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    private String truncate(String input) {
+        if (Objects.isNull(input)) {
+            return null;
+        }
+        return StringUtils.truncate(input, truncationLength);
+    }
+
 }
--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/exception/GlobalExceptionHandlerTest.java
@@ -2,24 +2,63 @@ package uk.gov.companieshouse.overseasentitiesapi.exception;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.context.request.WebRequest;
+import uk.gov.companieshouse.api.util.security.EricConstants;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static uk.gov.companieshouse.overseasentitiesapi.utils.Constants.ERIC_REQUEST_ID_KEY;
 
+@ExtendWith(MockitoExtension.class)
 class GlobalExceptionHandlerTest {
 
+    public static final String REQUEST_ID = "1234";
     private GlobalExceptionHandler globalExceptionHandler;
+
+    @Mock
+    private WebRequest webRequest;
+
+//    @Mock
+//    private Exception exception;
 
     @BeforeEach
     void setUp() {
         this.globalExceptionHandler = new GlobalExceptionHandler();
+        setTruncationLength(1000);
+    }
+
+    private void setTruncationLength(int length) {
+        ReflectionTestUtils.setField(globalExceptionHandler, "truncationLength", length);
     }
 
     @Test
-    void testHandleException() {
-        ResponseEntity<Object> entity = globalExceptionHandler.handleException(new Exception());
+    void testHandleExceptionReturnsCorrectResponse() {
+        when(webRequest.getHeader(ERIC_REQUEST_ID_KEY)).thenReturn(REQUEST_ID);
+
+        ResponseEntity<Object> entity = globalExceptionHandler.handleException(new Exception(), webRequest);
+
+        assertNotNull(entity);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, entity.getStatusCode());
+    }
+
+    @Test
+    void testHandleExceptionEncodesException() {
+        Throwable rootCause = new Throwable("root cause");
+        Exception exception = new Exception("exception message", rootCause);
+
+        when(webRequest.getHeader(ERIC_REQUEST_ID_KEY)).thenReturn(REQUEST_ID);
+
+        ResponseEntity<Object> entity = globalExceptionHandler.handleException(exception, webRequest);
 
         assertNotNull(entity);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, entity.getStatusCode());
