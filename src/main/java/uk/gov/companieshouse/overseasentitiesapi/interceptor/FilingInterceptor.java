a311360355a0d4feec7191726fc701032fef5adc
mwestacott <mwestacott2@companieshouse.gov.uk>
2022-12-22 11:02:00 +0000

    ROE-1778 Only allow updates against
    https://companieshouse.atlassian.net/browse/ROE-1778
    
    * Filing interceptor works as before, but implementation changed slightly
    in that class now takes ownership of checking the transaction status as
    well as handling it
    * Processing interceptor implementation changed in a similar way and now
    also only allows requests against OPEN transactions
    * New processing interceptor tests added

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/interceptor/FilingInterceptor.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/interceptor/FilingInterceptor.java
@@ -1,27 +1,29 @@
 package uk.gov.companieshouse.overseasentitiesapi.interceptor;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.overseasentitiesapi.utils.ApiLogger;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
+import static uk.gov.companieshouse.api.model.transaction.TransactionStatus.CLOSED;
+
 /**
  * A request interceptor class that checks if a request to retrieve filing data is allowed - for this to be true a
- * transaction must be present in the request attributes and that transaction must be CLOSED.
+ * transaction must be present in the request attributes and that transaction must be 'closed'.
  */
 @Component
-public class FilingInterceptor extends AbstractClosedTransactionInterceptor {
+public class FilingInterceptor extends AbstractTransactionStatusInterceptor {
 
     @Override
-    boolean handleClosedTransactionStatus(String reqId, HashMap<String, Object> logMap, HttpServletResponse response) {
-        ApiLogger.infoContext(reqId, "Transaction is closed - filing allowed", logMap);
+    boolean handleTransactionStatus(Transaction transaction, String reqId, HashMap<String, Object> logMap, HttpServletResponse response) {
+        if (CLOSED.equals(transaction.getStatus())) {
+            ApiLogger.infoContext(reqId, "Transaction is closed - filing allowed", logMap);
 
-        return true;
-    }
+            return true;
+        }
 
-    @Override
-    boolean handleNonClosedTransactionStatus(String reqId, HashMap<String, Object> logMap, HttpServletResponse response) {
         ApiLogger.errorContext(reqId, "Transaction is not closed - filing disallowed", null, logMap);
 
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
