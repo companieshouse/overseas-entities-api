07af6900cb84aeefa5f0e91032efd881425cb374
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-03-28 11:47:12 +0100

    ROE-2075 Changes to transformer v1.0
    * Also removed the dummy transformer which is no longer required

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/converter/Version1_0Transformer.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/converter/Version1_0Transformer.java
@@ -1,9 +1,12 @@
 package uk.gov.companieshouse.overseasentitiesapi.converter;
 
 import org.bson.Document;
+import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.overseasentitiesapi.model.SchemaVersion;
+import uk.gov.companieshouse.overseasentitiesapi.utils.ApiLogger;
 
-public class Version1_0Transformer implements Transformer {
+@Component
+public class Version1_0Transformer implements DocumentTransformer {
 
     private static final String ENTITY_BLOCK_NAME = "entity";
     private static final String OLD_ENTITY_NAME_FIELD_NAME = "name";
@@ -12,11 +15,13 @@ public class Version1_0Transformer implements Transformer {
 
     @Override
     public SchemaVersion forSchemaVersion() {
-        return null; // SchemaVersion.VERSION_1_0;
+        return SchemaVersion.VERSION_1_0;
     }
 
     @Override
     public void transform(Document submissionDocument) {
+        ApiLogger.info("Transforming a version 1.0 document...");
+
         Document entityDocument = (Document) submissionDocument.get(ENTITY_BLOCK_NAME);
         String entityName = entityDocument.getString(OLD_ENTITY_NAME_FIELD_NAME);
         Document entityNameDocument = new Document();
