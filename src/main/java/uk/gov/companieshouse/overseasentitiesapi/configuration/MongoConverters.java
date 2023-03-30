0f8d38da62b9726aaeb43b2d7d55663b0ab2a54e
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-03-30 09:45:36 +0100

    ROE-2100 Fix the warning shown when converting
    * JVM warning no longer displays when performing a default
    model conversion
    * Default mapping context (as opposed to a new one) now used
    when creating the new default converter

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/configuration/MongoConverters.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/configuration/MongoConverters.java
@@ -1,14 +1,14 @@
 package uk.gov.companieshouse.overseasentitiesapi.configuration;
 
+import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import uk.gov.companieshouse.overseasentitiesapi.converter.OverseasEntitySubmissionDaoConverter;
 import uk.gov.companieshouse.overseasentitiesapi.converter.DocumentTransformerFactory;
+import uk.gov.companieshouse.overseasentitiesapi.converter.OverseasEntitySubmissionDaoConverter;
 import uk.gov.companieshouse.overseasentitiesapi.utils.ApiLogger;
 
 import java.util.List;
@@ -18,17 +18,18 @@ public class MongoConverters {
     private MongoConverters() { }
 
     public static MongoCustomConversions getMongoCustomConversions(MongoDatabaseFactory mongoDatabaseFactory,
-                                                                   DocumentTransformerFactory transformerFactory) {
+                                                                   DocumentTransformerFactory transformerFactory,
+                                                                   MappingContext mappingContext) {
         ApiLogger.info("Adding Mongo Custom Converters - OverseasEntitySubmissionDaoConverter");
         return new MongoCustomConversions(
                 List.of(
-                        new OverseasEntitySubmissionDaoConverter(getDefaultMongoConverter(mongoDatabaseFactory), transformerFactory)
+                        new OverseasEntitySubmissionDaoConverter(getDefaultMongoConverter(mongoDatabaseFactory, mappingContext), transformerFactory)
                 ));
     }
 
-    private static MongoConverter getDefaultMongoConverter(MongoDatabaseFactory factory) {
+    private static MongoConverter getDefaultMongoConverter(MongoDatabaseFactory factory, MappingContext mappingContext) {
         DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
-        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, new MongoMappingContext());
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mappingContext);
         converter.afterPropertiesSet();
         return converter;
     }
