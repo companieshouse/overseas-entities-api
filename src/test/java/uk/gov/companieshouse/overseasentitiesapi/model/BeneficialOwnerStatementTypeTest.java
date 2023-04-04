e4d6ad74718fee5da7da98d9f603e82c29c2206e
mattch1 <mculpin@companieshouse.gov.uk>
2023-04-04 11:52:40 +0100

    ROE-2092 Add test parameters

--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/model/BeneficialOwnerStatementTypeTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/model/BeneficialOwnerStatementTypeTest.java
@@ -1,39 +1,29 @@
 package uk.gov.companieshouse.overseasentitiesapi.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class BeneficialOwnerStatementTypeTest {
 
-    @Test
-    void checkFindByBeneficialOwnersStatementTypeStringLowerCase() {
-        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString("all_identified_all_details");
+    @ParameterizedTest
+    @ValueSource(strings = { "all_identified_all_details", "ALL_IDENTIFIED_ALL_DETAILS" } )
+    void checkFindByBeneficialOwnersStatementTypeStringExpectedEnumCases(String enumInput) {
+        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString(enumInput);
         assertEquals(BeneficialOwnersStatementType.ALL_IDENTIFIED_ALL_DETAILS, type);
     }
 
-    @Test
-    void checkFindByBeneficialOwnersStatementTypeStringUpperCase() {
-        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString("ALL_IDENTIFIED_ALL_DETAILS");
-        assertEquals(BeneficialOwnersStatementType.ALL_IDENTIFIED_ALL_DETAILS, type);
-    }
-
-    @Test
-    void checkFindByBeneficialOwnersStatementTypeStringNull() {
-        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString(null);
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = { " ", "Batman" } )
+    void checkFindByBeneficialOwnersStatementTypeStringNullCases(String enumInput) {
+        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString(enumInput);
         assertNull(type);
     }
 
-    @Test
-    void checkFindByBeneficialOwnersStatementTypeStringEmpty() {
-        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString("");
-        assertNull(type);
-    }
 
-    @Test
-    void checkFindByBeneficialOwnersStatementTypeStringNonMatching() {
-        BeneficialOwnersStatementType type = BeneficialOwnersStatementType.findByBeneficialOwnersStatementTypeString("Batman");
-        assertNull(type);
-    }
 }
