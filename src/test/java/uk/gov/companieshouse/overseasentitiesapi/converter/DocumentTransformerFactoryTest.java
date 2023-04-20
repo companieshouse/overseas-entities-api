3b9b2892c5fdddd006c4bcf530d8fb631c2d139c
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-04-20 11:11:34 +0100

    ROE-2020 Fix issue with pipeline test failure
    * Test only failed during build-test-analyse phase of the pipeline
    (for some unknown reason)
    * Factory instantiation and initialisation approach changed in an
    effort to fix this

--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/converter/DocumentTransformerFactoryTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/converter/DocumentTransformerFactoryTest.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.overseasentitiesapi.converter;
 
 import org.bson.Document;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.companieshouse.overseasentitiesapi.model.SchemaVersion;
@@ -15,19 +14,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DocumentTransformerFactoryTest {
 
-    private DocumentTransformerFactory transformerFactory;
-
-    @BeforeEach
-    void initialise() {
-        transformerFactory = new DocumentTransformerFactory();
-        List<DocumentTransformer> transformers = new ArrayList<>();
-        transformers.add(new TestTransformer());
-        ReflectionTestUtils.setField(transformerFactory, "transformers", transformers);
-    }
-
     @Test
     void testTransformerFactoryWhenTransformerFound() {
-        transformerFactory.initialiseTransformerMap();
+        DocumentTransformerFactory transformerFactory = getDocumentTransformerFactory();
         Optional<DocumentTransformer> transformer = transformerFactory.getTransformer(SchemaVersion.VERSION_3_1);
         assertTrue(transformer.isPresent());
         assertEquals(SchemaVersion.VERSION_3_1, transformer.get().forSchemaVersion());
@@ -35,9 +24,21 @@ class DocumentTransformerFactoryTest {
 
     @Test
     void testTransformerFactoryWhenTransformerNotFound() {
-        transformerFactory.initialiseTransformerMap();
+        DocumentTransformerFactory transformerFactory = getDocumentTransformerFactory();
         Optional<DocumentTransformer> transformer = transformerFactory.getTransformer(SchemaVersion.VERSION_1_0);
         assertTrue(transformer.isEmpty());
+    }
+
+    // This approach for initialising the test instance of the DocumentTransformerFactory has been used as pipeline test
+    // failures were encountered when using the JUnit @BeforeEach annotation to create the factory
+    private DocumentTransformerFactory getDocumentTransformerFactory() {
+        DocumentTransformerFactory transformerFactory = new DocumentTransformerFactory();
+        List<DocumentTransformer> transformers = new ArrayList<>();
+        transformers.add(new TestTransformer());
+        ReflectionTestUtils.setField(transformerFactory, "transformers", transformers);
+        transformerFactory.initialiseTransformerMap();
+
+        return transformerFactory;
     }
 
     public class TestTransformer implements DocumentTransformer {
