0993d9ebc6c6fe9624f4ca6d2aed30866aa046cb
“vgummadala” <vgummadala@companieshouse.gov.uk>
2023-05-17 14:14:45 +0100

    Changes post styling using google-java-format plugin & changes to BeneficialOwnerStatementType

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/service/update/PopulateUpdateSubmission.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/service/update/PopulateUpdateSubmission.java
@@ -1,7 +1,5 @@
 package uk.gov.companieshouse.overseasentitiesapi.service.update;
 
-import org.apache.commons.lang.StringUtils;
-import uk.gov.companieshouse.overseasentitiesapi.model.dto.BeneficialOwnerCorporateDto;
 import uk.gov.companieshouse.overseasentitiesapi.model.dto.DueDiligenceDto;
 import uk.gov.companieshouse.overseasentitiesapi.model.dto.OverseasEntitySubmissionDto;
 import uk.gov.companieshouse.overseasentitiesapi.model.updatesubmission.DueDiligence;
@@ -9,112 +7,93 @@ import uk.gov.companieshouse.overseasentitiesapi.model.updatesubmission.FilingFo
 import uk.gov.companieshouse.overseasentitiesapi.model.updatesubmission.Presenter;
 import uk.gov.companieshouse.overseasentitiesapi.model.updatesubmission.UpdateSubmission;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
 /**
- * This is a helper class for FilingsService when the filing is an update.
- * The purpose of this class is to populate change submission values into
- * UpdateSubmission.java with the ultimate gaol to produce a json output of
- * change submissions. It uses  OESubmissionDTO and UpdateDTO values to populate
- * JSON template.
+ * This is a helper class for FilingsService when the filing is an update. The purpose of this class
+ * is to populate change submission values into UpdateSubmission.java with the ultimate gaol to
+ * produce a json output of change submissions. It uses OESubmissionDTO and UpdateDTO values to
+ * populate JSON template.
  */
 public class PopulateUpdateSubmission {
-    private OverseasEntitySubmissionDto overseasEntitySubmissionDto;
-    private UpdateSubmission updateSubmission;
+  private OverseasEntitySubmissionDto overseasEntitySubmissionDto;
+  private UpdateSubmission updateSubmission;
 
-    /* No arg constructor*/
-    public PopulateUpdateSubmission() {
+  public PopulateUpdateSubmission(
+      OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
+    this.overseasEntitySubmissionDto = overseasEntitySubmissionDto;
+    this.updateSubmission = updateSubmission;
+  }
 
-    }
-
-    public PopulateUpdateSubmission(OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
-        this.overseasEntitySubmissionDto    = overseasEntitySubmissionDto;
-        this.updateSubmission               = updateSubmission;
-    }
-
-    /* method populates values into UpdateSubmission for JSON output */
-public UpdateSubmission populate() {
-    //updateSubmission.type(); Type is already set in UserSubmission, has a default value of "OE02";
+  /* method populates values into UpdateSubmission for JSON output */
+  public UpdateSubmission populate() {
+    // updateSubmission.type(); Type is already set in UserSubmission, has a default value of
+    // "OE02";
     updateSubmission.setUserSubmission(this.overseasEntitySubmissionDto);
     populateDueDiligence(this.overseasEntitySubmissionDto, updateSubmission);
     populatePresenter(this.overseasEntitySubmissionDto, this.updateSubmission);
-    // BOStatement TBD, already contacted Matthew Culpin to over-ride toString below//
     updateSubmission.setBeneficialOwnerStatement(
-            this.overseasEntitySubmissionDto.getBeneficialOwnersStatement().toString());
+        this.overseasEntitySubmissionDto.getBeneficialOwnersStatement().getValue());
 
     updateSubmission.setAnyBOsOrMOsAddedOrCeased(
-            this.overseasEntitySubmissionDto.getUpdate()
-                    .isRegistrableBeneficialOwner()
-    );
-    //contact details, --> overseasEntitySubmissionDto.getPresenter().getEmail();
+        this.overseasEntitySubmissionDto.getUpdate().isRegistrableBeneficialOwner());
+    // AC contact details is covered in --> overseasEntitySubmissionDto.getPresenter().getEmail();
 
-
-    // TBD: NoChangesInFilingPeriodStatement will be done in https://companieshouse.atlassian.net/browse/UAR-461//
+    // TBD: NoChangesInFilingPeriodStatement will be done in
+    // https://companieshouse.atlassian.net/browse/UAR-461//
     // TBD updateSubmission.setNoChangesInFilingPeriodStatement(); //
 
     return updateSubmission;
-}
+  }
 
-public void populateDueDiligence(OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
-    DueDiligenceDto         dueDiligenceDto     = overseasEntitySubmissionDto.getDueDiligence(); //TBD .. which DueDiligence to use??//
-    DueDiligence            dueDiligence        = new DueDiligence();
+  public void populateDueDiligence(
+      OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
+    DueDiligenceDto dueDiligenceDto =
+        overseasEntitySubmissionDto.getDueDiligence(); // TBD .. which DueDiligence to use??//
+    DueDiligence dueDiligence = new DueDiligence();
     dueDiligence.setDueDiligenceCorrespondenceAddress(dueDiligenceDto.getAddress());
     dueDiligence.setAgentName(dueDiligenceDto.getName());
     dueDiligence.setEmail(dueDiligenceDto.getEmail());
-    dueDiligence.setDateChecked(dueDiligenceDto.getIdentityDate().toString()); //TBD .. check date format//
+    dueDiligence.setDateChecked(
+        dueDiligenceDto.getIdentityDate().toString()); // TBD .. check date format//
     dueDiligence.setPartnerName(dueDiligenceDto.getPartnerName());
     dueDiligence.setAgentAssuranceCode(dueDiligenceDto.getAgentCode());
     dueDiligence.setSupervisoryBody(dueDiligenceDto.getSupervisoryName());
     dueDiligence.setAmlRegistrationNumber(dueDiligenceDto.getAmlNumber());
     updateSubmission.setDueDiligence(dueDiligence);
-}
+  }
 
-    public void populateFilingForDate(OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
-        FilingForDate filingForDate = new FilingForDate();
-        var filingDate              = overseasEntitySubmissionDto.getUpdate().getFilingDate();
-        filingForDate.setDay((
-                String.valueOf(
-                        filingDate.getDayOfMonth())));
-        filingForDate.setMonth((
-                String.valueOf(
-                        filingDate.getMonth())));
-        filingForDate.setYear((
-                String.valueOf(
-                        filingDate.getYear())));
-        updateSubmission.setFilingForDate(filingForDate);
+  public void populateFilingForDate(
+      OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
+    FilingForDate filingForDate = new FilingForDate();
+    var filingDate = overseasEntitySubmissionDto.getUpdate().getFilingDate();
+    filingForDate.setDay((String.valueOf(filingDate.getDayOfMonth())));
+    filingForDate.setMonth((String.valueOf(filingDate.getMonth())));
+    filingForDate.setYear((String.valueOf(filingDate.getYear())));
+    updateSubmission.setFilingForDate(filingForDate);
+  }
 
-    }
-    public void populatePresenter(OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
-        Presenter           presenter           = new Presenter();
-        presenter.setEmail(overseasEntitySubmissionDto.getPresenter().getEmail());
-        presenter.setName(overseasEntitySubmissionDto.getPresenter().getFullName());
-        updateSubmission.setPresenter(presenter);
-    }
+  public void populatePresenter(
+      OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
+    Presenter presenter = new Presenter();
+    presenter.setEmail(overseasEntitySubmissionDto.getPresenter().getEmail());
+    presenter.setName(overseasEntitySubmissionDto.getPresenter().getFullName());
+    updateSubmission.setPresenter(presenter);
+  }
+
+  public UpdateSubmission populate(
+      OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
+    this.overseasEntitySubmissionDto = overseasEntitySubmissionDto;
+    this.updateSubmission = updateSubmission;
+    return populate();
+  }
+
+  public OverseasEntitySubmissionDto getOverseasEntitySubmissionDto() {
+    return overseasEntitySubmissionDto;
+  }
+
+  public void setOverseasEntitySubmissionDto(
+      OverseasEntitySubmissionDto overseasEntitySubmissionDto) {
+    this.overseasEntitySubmissionDto = overseasEntitySubmissionDto;
+  }
 
 
-
-
-
-    public UpdateSubmission populate(OverseasEntitySubmissionDto overseasEntitySubmissionDto, UpdateSubmission updateSubmission) {
-        this.overseasEntitySubmissionDto    = overseasEntitySubmissionDto;
-        this.updateSubmission               = updateSubmission;
-        return   populate();
-    }
-
-
-    public OverseasEntitySubmissionDto getOverseasEntitySubmissionDto() {
-        return overseasEntitySubmissionDto;
-    }
-
-    public void setOverseasEntitySubmissionDto(OverseasEntitySubmissionDto overseasEntitySubmissionDto) {
-        this.overseasEntitySubmissionDto = overseasEntitySubmissionDto;
-
-    }
-
-    @Override
-    public String toString() {
-        return "PopulateUpdateSubmission{}";
-    }
 }
