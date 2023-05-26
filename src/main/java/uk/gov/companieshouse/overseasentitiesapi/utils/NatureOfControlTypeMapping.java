4792b25139b8d0639ff810ac749498cf78112ca1
bbell-companieshouse <bbell@companieshouse.gov.uk>
2023-05-26 14:08:43 +0100

    UAR-581: Add enum class

---
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/NatureOfControlTypeMapping.java
@@ -0,0 +1,41 @@
+package uk.gov.companieshouse.overseasentitiesapi.utils;
+
+import uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.OVER_25_PERCENT_OF_SHARES;
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL;
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS;
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS;
+
+public class NatureOfControlTypeMapping {
+    public static final Map<NatureOfControlType, String> OVERSEAS_ENTITIES_PERSON_MAP;
+    public static final Map<NatureOfControlType, String> OVERSEAS_ENTITIES_TRUST_MAP;
+    public static final Map<NatureOfControlType, String> OVERSEAS_ENTITIES_FIRM_MAP;
+
+    static {
+        OVERSEAS_ENTITIES_PERSON_MAP = new EnumMap<>(NatureOfControlType.class);
+        OVERSEAS_ENTITIES_PERSON_MAP.put(OVER_25_PERCENT_OF_SHARES, "OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_PERSON");
+        OVERSEAS_ENTITIES_PERSON_MAP.put(OVER_25_PERCENT_OF_VOTING_RIGHTS, "OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_PERSON");
+        OVERSEAS_ENTITIES_PERSON_MAP.put(APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, "OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_PERSON");
+        OVERSEAS_ENTITIES_PERSON_MAP.put(SIGNIFICANT_INFLUENCE_OR_CONTROL, "OE_SIGINFLUENCECONTROL_AS_PERSON");
+
+        OVERSEAS_ENTITIES_TRUST_MAP = new EnumMap<>(NatureOfControlType.class);
+        OVERSEAS_ENTITIES_TRUST_MAP.put(OVER_25_PERCENT_OF_SHARES, "OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_TRUST");
+        OVERSEAS_ENTITIES_TRUST_MAP.put(OVER_25_PERCENT_OF_VOTING_RIGHTS, "OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_TRUST");
+        OVERSEAS_ENTITIES_TRUST_MAP.put(APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, "OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_TRUST");
+        OVERSEAS_ENTITIES_TRUST_MAP.put(SIGNIFICANT_INFLUENCE_OR_CONTROL, "OE_SIGINFLUENCECONTROL_AS_TRUST");
+
+        OVERSEAS_ENTITIES_FIRM_MAP = new EnumMap<>(NatureOfControlType.class);
+        OVERSEAS_ENTITIES_FIRM_MAP.put(OVER_25_PERCENT_OF_SHARES, "OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_FIRM");
+        OVERSEAS_ENTITIES_FIRM_MAP.put(OVER_25_PERCENT_OF_VOTING_RIGHTS, "OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_FIRM");
+        OVERSEAS_ENTITIES_FIRM_MAP.put(APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, "OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_FIRM");
+        OVERSEAS_ENTITIES_FIRM_MAP.put(SIGNIFICANT_INFLUENCE_OR_CONTROL, "OE_SIGINFLUENCECONTROL_AS_FIRM");
+    }
+
+    private NatureOfControlTypeMapping() {
+        throw new IllegalAccessError("Use the static method designation");
+    }
+}
