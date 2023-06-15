68bf46f89c6d1c6f4f0f15fed5487fa2148393e1
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-06-15 16:44:23 +0100

    uar-591 presenter name to fullName

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/PopulateUpdateSubmission.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/utils/PopulateUpdateSubmission.java
@@ -116,7 +116,7 @@ public class PopulateUpdateSubmission {
                             .ifPresent(presenter::setEmail);
                     Optional.of(presenterDto).
                             map(PresenterDto::getFullName)
-                            .ifPresent(presenter::setName);
+                            .ifPresent(presenter::setFullName);
                     Optional.of(presenter)
                             .ifPresent(updateSubmission::setPresenter);
                 });
