cce7e64759cf8eacded305e39a6513cf211d1212
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-06-22 11:55:32 +0100

    uar-616 use overseas entity submission

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/controller/OverseasEntitiesDataController.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/controller/OverseasEntitiesDataController.java
@@ -1,52 +1,96 @@
 package uk.gov.companieshouse.overseasentitiesapi.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.api.model.update.OverseasEntityDataApi;
 import uk.gov.companieshouse.overseasentitiesapi.exception.ServiceException;
+import uk.gov.companieshouse.overseasentitiesapi.model.dto.OverseasEntitySubmissionDto;
+import uk.gov.companieshouse.overseasentitiesapi.service.OverseasEntitiesService;
 import uk.gov.companieshouse.overseasentitiesapi.service.PrivateDataRetrievalService;
 import uk.gov.companieshouse.overseasentitiesapi.utils.ApiLogger;
 import java.util.HashMap;
 
 import static uk.gov.companieshouse.overseasentitiesapi.utils.Constants.ERIC_REQUEST_ID_KEY;
+import static uk.gov.companieshouse.overseasentitiesapi.utils.Constants.OVERSEAS_ENTITY_ID_KEY;
+import static uk.gov.companieshouse.overseasentitiesapi.utils.Constants.TRANSACTION_ID_KEY;
+import static uk.gov.companieshouse.overseasentitiesapi.utils.Constants.TRANSACTION_KEY;
 
 @RestController
 @RequestMapping("/overseas-entity-details")
 public class OverseasEntitiesDataController {
 
     PrivateDataRetrievalService privateDataRetrievalService;
+    OverseasEntitiesService overseasEntitiesService;
 
     @Value("${FEATURE_FLAG_ENABLE_ROE_UPDATE_24112022:false}")
     private boolean isRoeUpdateEnabled;
 
     @Autowired
-    public OverseasEntitiesDataController(final PrivateDataRetrievalService privateDataRetrievalService){
+    public OverseasEntitiesDataController(
+            final PrivateDataRetrievalService privateDataRetrievalService,
+            final OverseasEntitiesService overseasEntitiesService
+    ){
         this.privateDataRetrievalService = privateDataRetrievalService;
+        this.overseasEntitiesService = overseasEntitiesService;
     }
 
-    @GetMapping("/{company_number}")
-    public ResponseEntity<Object> getOverseasEntityDetails (@PathVariable("company_number") String companyNumber,
-                                                            @RequestHeader(value = ERIC_REQUEST_ID_KEY) String requestId) throws ServiceException {
+    @GetMapping("/{overseas_entity_id}")
+    public ResponseEntity<Object> getOverseasEntityDetails (
+            @RequestAttribute(TRANSACTION_KEY) Transaction transaction,
+            @PathVariable(OVERSEAS_ENTITY_ID_KEY) String overseasEntityId,
+            @RequestHeader(value = ERIC_REQUEST_ID_KEY) String requestId) throws ServiceException {
+
         var logMap = new HashMap<String, Object>();
-        logMap.put("Company Number", companyNumber);
+        logMap.put(OVERSEAS_ENTITY_ID_KEY, overseasEntityId);
+        logMap.put(TRANSACTION_ID_KEY, transaction.getId());
 
-        if (!isRoeUpdateEnabled) {
-            throw new ServiceException("ROE_UPDATE must be enabled for overseas entity details");
-        }
+        ApiLogger.infoContext(requestId, "Calling service to check the overseas entity submission", logMap);
+        var submissionDtoOptional = overseasEntitiesService.getOverseasEntitySubmission(overseasEntityId);
+        if (submissionDtoOptional.isPresent()) {
+            OverseasEntitySubmissionDto submissionDto = submissionDtoOptional.get();
+            if (!submissionDto.isForUpdate()) {
+                throw new ServiceException("Submission for overseas entity details must be for update");
+            }
+            if (!isRoeUpdateEnabled) {
+                throw new ServiceException("ROE Update feature must be enabled for get overseas entity details");
+            }
 
-        try {
-            ApiLogger.infoContext(requestId, "Calling service to get OE details", logMap);
-            return ResponseEntity.ok(privateDataRetrievalService.getOverseasEntityData(companyNumber));
-        } catch (ServiceException e) {
-            ApiLogger.errorContext(requestId, e.getMessage(), e, logMap);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            String email = submissionDto.getEntity().getEmail();
+
+            OverseasEntityDataApi overseasEntityDataApi = null;
+
+            if (StringUtils.isBlank(email)) {
+                String companyNumber = submissionDto.getEntityNumber();
+
+                try {
+                    ApiLogger.infoContext(requestId, "Calling service to get OE details", logMap);
+                    overseasEntityDataApi = privateDataRetrievalService.getOverseasEntityData(companyNumber);
+
+                } catch (ServiceException e) {
+                    ApiLogger.errorContext(requestId, e.getMessage(), e, logMap);
+                    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                overseasEntityDataApi.setEmail(email);
+            }
+
+            return ResponseEntity.ok(overseasEntityDataApi);
+
+        } else {
+            final var message = String.format("Could not find overseas entity submission %s", overseasEntityId);
+            ApiLogger.errorContext(requestId, message, null, logMap);
+            return ResponseEntity.notFound().build();
         }
     }
 }
--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/controller/OverseasEntitiesDataControllerTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/controller/OverseasEntitiesDataControllerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.overseasentitiesapi.controller;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -7,8 +8,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.api.model.update.OverseasEntityDataApi;
 import uk.gov.companieshouse.overseasentitiesapi.exception.ServiceException;
+import uk.gov.companieshouse.overseasentitiesapi.model.dto.EntityDto;
+import uk.gov.companieshouse.overseasentitiesapi.model.dto.OverseasEntitySubmissionDto;
+import uk.gov.companieshouse.overseasentitiesapi.service.OverseasEntitiesService;
 import uk.gov.companieshouse.overseasentitiesapi.service.PrivateDataRetrievalService;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,34 +23,49 @@ import static org.mockito.Mockito.*;
 class OverseasEntitiesDataControllerTest {
 
     private static final String ERIC_REQUEST_ID = "XaBcDeF12345";
-    private static final String companyNumber = "OE000201";
-    private static String email = "TEST@MAILINATOR.COM";
+    private static final String overseasEntityId = "123456778";
+    private static final String transactionId = "abcdef";
+    private static final String email = "TEST@MAILINATOR.COM";
+
+    private static final String COMPANY_NUMBER = "OE111129";
 
     @Mock
     private PrivateDataRetrievalService privateDataRetrievalService;
+
+    @Mock
+    private OverseasEntitiesService overseasEntitiesService;
 
     @Test
     void testGetOverseasEntityDetailsReturnsSuccessfully () throws ServiceException {
         OverseasEntityDataApi overseasEntityDataApi = new OverseasEntityDataApi();
         overseasEntityDataApi.setEmail(email);
-        when(privateDataRetrievalService.getOverseasEntityData(companyNumber)).thenReturn(overseasEntityDataApi);
+        when(privateDataRetrievalService.getOverseasEntityData(any())).thenReturn(overseasEntityDataApi);
+        when(overseasEntitiesService.getOverseasEntitySubmission(overseasEntityId)).thenReturn(
+                Optional.of(createOverseasEntitySubmissionMock()));
 
-        OverseasEntitiesDataController overseasEntitiesDataController = new OverseasEntitiesDataController(privateDataRetrievalService);
+        OverseasEntitiesDataController overseasEntitiesDataController = new OverseasEntitiesDataController(privateDataRetrievalService, overseasEntitiesService);
         setUpdateEnabledFeatureFlag(overseasEntitiesDataController, true);
-        var response = overseasEntitiesDataController.getOverseasEntityDetails(companyNumber, ERIC_REQUEST_ID);
+        Transaction transaction = new Transaction();
+        transaction.setId(transactionId);
+        var response = overseasEntitiesDataController.getOverseasEntityDetails(transaction, overseasEntityId, ERIC_REQUEST_ID);
 
-        verify(privateDataRetrievalService, times(1)).getOverseasEntityData(companyNumber);
+        verify(privateDataRetrievalService, times(1)).getOverseasEntityData(COMPANY_NUMBER);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(overseasEntityDataApi, response.getBody());
     }
 
     @Test
     void testGetOverseasEntityDetailsReturnsInternalServerErrorWhenExceptionThrown () throws ServiceException {
-        Mockito.doThrow(new ServiceException("Exception thrown")).when(privateDataRetrievalService).getOverseasEntityData(companyNumber);
+        Mockito.doThrow(new ServiceException("Exception thrown")).when(privateDataRetrievalService).getOverseasEntityData(COMPANY_NUMBER);
 
-        OverseasEntitiesDataController overseasEntitiesDataController = new OverseasEntitiesDataController(privateDataRetrievalService);
+        when(overseasEntitiesService.getOverseasEntitySubmission(overseasEntityId)).thenReturn(
+                Optional.of(createOverseasEntitySubmissionMock()));
+
+        OverseasEntitiesDataController overseasEntitiesDataController = new OverseasEntitiesDataController(privateDataRetrievalService, overseasEntitiesService);
         setUpdateEnabledFeatureFlag(overseasEntitiesDataController, true);
-        var response = overseasEntitiesDataController.getOverseasEntityDetails(companyNumber, ERIC_REQUEST_ID);
+        Transaction transaction = new Transaction();
+        transaction.setId(transactionId);
+        var response = overseasEntitiesDataController.getOverseasEntityDetails(transaction, overseasEntityId, ERIC_REQUEST_ID);
 
         assertNull(response.getBody());
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
@@ -55,12 +75,50 @@ class OverseasEntitiesDataControllerTest {
     void testGetOverseasEntityDetailsReturnsInternalServerErrorWhenUpdateDisabled () throws ServiceException {
 
         OverseasEntitiesDataController overseasEntitiesDataController = new OverseasEntitiesDataController(
-                privateDataRetrievalService);
+                privateDataRetrievalService, overseasEntitiesService);
         setUpdateEnabledFeatureFlag(overseasEntitiesDataController, false);
 
+        when(overseasEntitiesService.getOverseasEntitySubmission(overseasEntityId)).thenReturn(
+                Optional.of(createOverseasEntitySubmissionMock()));
+
+        Transaction transaction = new Transaction();
+        transaction.setId(transactionId);
+
         assertThrows(ServiceException.class,
-                () -> overseasEntitiesDataController.getOverseasEntityDetails(companyNumber,
+                () -> overseasEntitiesDataController.getOverseasEntityDetails(
+                        transaction,
+                        overseasEntityId,
                         ERIC_REQUEST_ID));
+    }
+
+    @Test
+    void testGetOverseasEntityDetailsReturnsInternalServerErrorWhenRegistration () throws ServiceException {
+
+        OverseasEntitiesDataController overseasEntitiesDataController = new OverseasEntitiesDataController(
+                privateDataRetrievalService, overseasEntitiesService);
+        setUpdateEnabledFeatureFlag(overseasEntitiesDataController, false);
+
+        OverseasEntitySubmissionDto overseasEntitySubmissionDto = createOverseasEntitySubmissionMock();
+        overseasEntitySubmissionDto.setEntityNumber(null);
+        when(overseasEntitiesService.getOverseasEntitySubmission(overseasEntityId)).thenReturn(
+                Optional.of(overseasEntitySubmissionDto));
+
+        Transaction transaction = new Transaction();
+        transaction.setId(transactionId);
+
+        assertThrows(ServiceException.class,
+                () -> overseasEntitiesDataController.getOverseasEntityDetails(
+                        transaction,
+                        overseasEntityId,
+                        ERIC_REQUEST_ID));
+    }
+
+    private OverseasEntitySubmissionDto createOverseasEntitySubmissionMock() {
+        OverseasEntitySubmissionDto overseasEntitySubmissionDto = new OverseasEntitySubmissionDto();
+        overseasEntitySubmissionDto.setEntityNumber(COMPANY_NUMBER);
+        EntityDto entityDto = new EntityDto();
+        overseasEntitySubmissionDto.setEntity(entityDto);
+        return overseasEntitySubmissionDto;
     }
 
     private void setUpdateEnabledFeatureFlag(OverseasEntitiesDataController overseasEntitiesDataController, boolean value) {
