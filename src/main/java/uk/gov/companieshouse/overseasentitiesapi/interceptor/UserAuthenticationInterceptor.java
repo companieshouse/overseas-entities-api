9b9c1549589b514d47586b5ded30ce67a55e928f
bantao-ch <bantao@companieshouse.gov.uk>
2023-06-30 12:03:36 +0100

    Add ROE Update Service Permissions -- Addressing Review Comments

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/interceptor/UserAuthenticationInterceptor.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/interceptor/UserAuthenticationInterceptor.java
@@ -35,8 +35,6 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
 
     private final TransactionService transactionService;
 
-    private String companyNumberInScope;
-
     @Autowired
     public UserAuthenticationInterceptor(TransactionService transactionService) {
         this.transactionService = transactionService;
@@ -68,24 +66,31 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
         // TokenPermissions should have been set up in the request by TokenPermissionsInterceptor
         final var tokenPermissions = getTokenPermissions(request)
                 .orElseThrow(() -> new IllegalStateException("UserAuthenticationInterceptor - TokenPermissions object not present in request"));
-
-        boolean isRoeUpdateJourneyRequest = isRequestRoeUpdate(request);
+        String companyNumberInScope = getCompanyNumberInScope(request);
+        boolean isRoeUpdateJourneyRequest = isRequestRoeUpdate(companyNumberInScope);
 
         var authInfoMap = new HashMap<String, Object>();
         authInfoMap.put(TRANSACTION_ID_KEY, transactionId);
         authInfoMap.put("request_method", request.getMethod());
+        authInfoMap.put("company_number_in_scope", companyNumberInScope);
         authInfoMap.put("is_roe_update_journey_request", isRoeUpdateJourneyRequest);
 
         if (isRoeUpdateJourneyRequest) {
-            boolean doCompanyNumbersMatch = doCompanyNumbersMatch(request, response, transactionId);
             // Check the user has the company_oe_annual_update=create permission for ROE Update
             boolean hasCompanyOeAnnualUpdateCreatePermission = tokenPermissions.hasPermission(Key.COMPANY_OE_ANNUAL_UPDATE, Value.CREATE);
             authInfoMap.put("has_company_oe_annual_update_create_permission", hasCompanyOeAnnualUpdateCreatePermission);
-            authInfoMap.put("roe_update_journey_company_numbers_match", doCompanyNumbersMatch);
-            if (hasCompanyOeAnnualUpdateCreatePermission && doCompanyNumbersMatch) {
-                ApiLogger.debugContext(reqId, "UserAuthenticationInterceptor authorised with company_oe_annual_update=create permission",
-                        authInfoMap);
-                return true;
+            if (hasCompanyOeAnnualUpdateCreatePermission) {
+                boolean doCompanyNumbersMatch = doCompanyNumbersMatch(request, response, companyNumberInScope, transactionId);
+                authInfoMap.put("roe_update_journey_company_numbers_match", doCompanyNumbersMatch);
+                if (doCompanyNumbersMatch) {
+                    ApiLogger.debugContext(reqId, "UserAuthenticationInterceptor authorised with company_oe_annual_update=create permission",
+                            authInfoMap);
+                    return true;
+                } else {
+                    ApiLogger.errorContext(reqId, "UserAuthenticationInterceptor unauthorised for ROE Update Journey as Company Numbers do not match", null, authInfoMap);
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return false;
+                }
             } else {
                 ApiLogger.errorContext(reqId, "UserAuthenticationInterceptor unauthorised for ROE Update Journey ", null, authInfoMap);
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -111,16 +116,19 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
         return AuthorisationUtil.getTokenPermissions(request);
     }
 
-    protected boolean isRequestRoeUpdate(HttpServletRequest request) {
-        final Map<String, List<String>> privileges = getERICTokenPermissions(request);
-        if (privileges.containsKey(COMPANY_NUMBER_KEY)) {
-            companyNumberInScope = privileges.get(COMPANY_NUMBER_KEY).get(0);
-            return StringUtils.isNotEmpty(companyNumberInScope) && companyNumberInScope.startsWith("OE");
-            }
-        return false;
+    private boolean isRequestRoeUpdate(String companyNumberInScope) {
+        return StringUtils.isNotEmpty(companyNumberInScope) && companyNumberInScope.startsWith("OE");
     }
 
-    protected boolean doCompanyNumbersMatch(HttpServletRequest request, HttpServletResponse response, String transactionId) {
+    private String getCompanyNumberInScope (HttpServletRequest request) {
+        final Map<String, List<String>> privileges = getERICTokenPermissions(request);
+        if (privileges.containsKey(COMPANY_NUMBER_KEY)) {
+            return privileges.get(COMPANY_NUMBER_KEY).get(0);
+        }
+        return null;
+    }
+
+    private boolean doCompanyNumbersMatch(HttpServletRequest request, HttpServletResponse response, String companyNumberInScope, String transactionId) {
         String companyNumberInTransaction = getCompanyNumberInTransaction(request, response, transactionId);
         if (StringUtils.isNotBlank(companyNumberInTransaction)) {
             return companyNumberInTransaction.equalsIgnoreCase(companyNumberInScope);
