18382e553d0c2df6ed83308bcdcedca127aec659
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-07-12 13:13:13 +0100

    uar-711 validate BO/MOs but not statement

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/validation/OverseasEntitySubmissionDtoValidator.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/validation/OverseasEntitySubmissionDtoValidator.java
@@ -78,15 +78,13 @@ public class OverseasEntitySubmissionDtoValidator {
             validateFullCommonDetails(overseasEntitySubmissionDto, errors, loggingContext);
 
             // Change to Statement Validation once BO/MO Statements are complete
-            // ownersAndOfficersDataBlockValidator.validateOwnersAndOfficers(overseasEntitySubmissionDto, errors, loggingContext);
+            ownersAndOfficersDataBlockValidator.validateOwnersAndOfficers(overseasEntitySubmissionDto, errors, loggingContext);
 
             dueDiligenceDataBlockValidator.validateFullDueDiligenceFields(
                     overseasEntitySubmissionDto.getDueDiligence(),
                     overseasEntitySubmissionDto.getOverseasEntityDueDiligence(),
                     errors,
                     loggingContext);
-
-            ownersAndOfficersDataBlockValidator.validateOwnersAndOfficersAgainstStatement(overseasEntitySubmissionDto, errors, loggingContext);
         }
         else {
             validateNoChangeUpdate(overseasEntitySubmissionDto, errors, loggingContext);
