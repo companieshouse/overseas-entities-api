3edce66608e86bb76be18d81786f6fda2f240c4a
bbell-companieshouse <bbell@companieshouse.gov.uk>
2023-09-26 09:17:26 +0100

    UAR-1145: Implement endpoint to retrieve all trust data for an overseas entity

--- src/main/java/uk/gov/companieshouse/overseasentitiesapi/service/PrivateDataRetrievalService.java
+++ src/main/java/uk/gov/companieshouse/overseasentitiesapi/service/PrivateDataRetrievalService.java
@@ -9,6 +9,7 @@ import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.beneficialowner.PrivateBoDataListApi;
 import uk.gov.companieshouse.api.model.managingofficerdata.ManagingOfficerListDataApi;
+import uk.gov.companieshouse.api.model.trusts.PrivateTrustDetailsListApi;
 import uk.gov.companieshouse.api.model.update.OverseasEntityDataApi;
 import uk.gov.companieshouse.overseasentitiesapi.client.ApiClientService;
 import uk.gov.companieshouse.overseasentitiesapi.exception.ServiceException;
@@ -118,6 +119,38 @@ public class PrivateDataRetrievalService {
       throw new ServiceException(e.getStatusMessage(), e);
     } catch (URIValidationException e) {
       var message = "Error Retrieving Beneficial Owner data for " + companyNumber;
+      ApiLogger.errorContext(message, e);
+      throw new ServiceException(e.getMessage(), e);
+    }
+  }
+
+  public PrivateTrustDetailsListApi getTrustDetails(String companyNumber) throws ServiceException {
+    var logMap = new HashMap<String, Object>();
+    logMap.put(COMPANY_NUMBER, companyNumber);
+    ApiLogger.info("Retrieving Trusts for Company Number " + companyNumber, logMap);
+
+    try {
+      PrivateTrustDetailsListApi trusts = apiClientService.getInternalApiClient()
+              .privateTrustsResourceHandler()
+              .getTrustDetails(OVERSEAS_ENTITY_URI_SECTION + companyNumber + "/trusts/details")
+              .execute()
+              .getData();
+
+      if (trusts != null && trusts.getTrustDetailsList() != null
+              && !trusts.getTrustDetailsList().isEmpty()) {
+        ApiLogger.info(String.format("Retrieved %d Trusts for Company Number %s",
+                trusts.getTrustDetailsList().size(), companyNumber));
+      }
+
+      return trusts;
+    } catch (ApiErrorResponseException e) {
+      if (e.getStatusCode() == HttpServletResponse.SC_NOT_FOUND) {
+        ApiLogger.info("No Trusts found for Company Number " + companyNumber, logMap);
+        return new PrivateTrustDetailsListApi(Collections.emptyList());
+      }
+      throw new ServiceException(e.getStatusMessage(), e);
+    } catch (URIValidationException e) {
+      var message = "Error Retrieving Trust data for " + companyNumber;
       ApiLogger.errorContext(message, e);
       throw new ServiceException(e.getMessage(), e);
     }
