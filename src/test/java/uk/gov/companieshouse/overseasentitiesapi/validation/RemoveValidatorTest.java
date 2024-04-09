93c4e44c1b50bbccc2f21c382a12b7d9ec52fd44
mattch1 <mculpin@companieshouse.gov.uk>
2024-04-09 13:47:11 +0100

    Test name change

--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/validation/RemoveValidatorTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/validation/RemoveValidatorTest.java
@@ -115,7 +115,7 @@ class RemoveValidatorTest {
     }
 
     @Test
-    void testFullValidationErrorReportedWhenFilingDateIsNotPresentAndRemoveBlockNotPresent() {
+    void testFullValidationErrorReportedWhenRemoveBlockNotPresent() {
         // Given
         overseasEntitySubmissionDto.setRemove(null);
 
