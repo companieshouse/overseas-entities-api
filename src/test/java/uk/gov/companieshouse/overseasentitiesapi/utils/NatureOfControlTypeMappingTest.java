c1ce17130eb01f9045ada53398a832a6ce8f5af0
mattch1 <mculpin@companieshouse.gov.uk>
2024-09-27 12:54:48 +0100

    UAR-1633 Unit test the mapping

--- src/test/java/uk/gov/companieshouse/overseasentitiesapi/utils/NatureOfControlTypeMappingTest.java
+++ src/test/java/uk/gov/companieshouse/overseasentitiesapi/utils/NatureOfControlTypeMappingTest.java
@@ -1,6 +1,7 @@
 package uk.gov.companieshouse.overseasentitiesapi.utils;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlJurisdictionType;
 import uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType;
 
 import java.lang.reflect.Constructor;
@@ -10,6 +11,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlJurisdictionType.ENGLAND_AND_WALES;
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlJurisdictionType.NORTHERN_IRELAND;
+import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlJurisdictionType.SCOTLAND;
 import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.OVER_25_PERCENT_OF_SHARES;
 import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS;
 import static uk.gov.companieshouse.overseasentitiesapi.model.NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS;
@@ -29,8 +33,9 @@ class NatureOfControlTypeMappingTest {
         assertEquals("Use the static method designation", exception.getCause().getMessage());
     }
 
+    // UAR-1583 remove when feature flag is removed
     @Test
-    void collectAllNatureOfControlsIntoSingleListAllValueNonNull() {
+    void collectAllNatureOfControlsIntoSingleListAllValueNonNullWhenNewNocFlagFalse() {
         List<NatureOfControlType> personNatureOfControlTypes =
                 Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
                         APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
@@ -41,10 +46,27 @@ class NatureOfControlTypeMappingTest {
                 Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
                         APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
 
+        // UAR-1583 New nocs
+        List<NatureOfControlType> trustControlNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+        List<NatureOfControlJurisdictionType> ownerOfLandPersonNatureOfControlJurisdictions =
+                Arrays.asList(ENGLAND_AND_WALES, SCOTLAND, NORTHERN_IRELAND);
+        List<NatureOfControlJurisdictionType> ownerOfLandOtherEntityNatureOfControlJurisdictions =
+                Arrays.asList(ENGLAND_AND_WALES, SCOTLAND, NORTHERN_IRELAND);
+        List<NatureOfControlType> firmControlNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+
         var result = collectAllNatureOfControlsIntoSingleList(
                 personNatureOfControlTypes,
                 trusteesNatureOfControlTypes,
-                firmNatureOfControlTypes);
+                firmNatureOfControlTypes,
+                trustControlNatureOfControlTypes,
+                ownerOfLandPersonNatureOfControlJurisdictions,
+                ownerOfLandOtherEntityNatureOfControlJurisdictions,
+                firmControlNatureOfControlTypes,
+                false);
 
         assertEquals(12, result.size());
         assertEquals("OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_PERSON", result.get(0));
@@ -59,6 +81,67 @@ class NatureOfControlTypeMappingTest {
         assertEquals("OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_FIRM", result.get(9));
         assertEquals("OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_FIRM", result.get(10));
         assertEquals("OE_SIGINFLUENCECONTROL_AS_FIRM", result.get(11));
+    }
+
+    @Test
+    void collectAllNatureOfControlsIntoSingleListAllValueNonNullWhenNewNocFlagTrue() {
+        List<NatureOfControlType> personNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+        List<NatureOfControlType> trusteesNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+        List<NatureOfControlType> firmNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+        List<NatureOfControlType> trustControlNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+        List<NatureOfControlJurisdictionType> ownerOfLandPersonNatureOfControlJurisdictions =
+                Arrays.asList(ENGLAND_AND_WALES, SCOTLAND, NORTHERN_IRELAND);
+        List<NatureOfControlJurisdictionType> ownerOfLandOtherEntityNatureOfControlJurisdictions =
+                Arrays.asList(ENGLAND_AND_WALES, SCOTLAND, NORTHERN_IRELAND);
+        List<NatureOfControlType> firmControlNatureOfControlTypes =
+                Arrays.asList(OVER_25_PERCENT_OF_SHARES, OVER_25_PERCENT_OF_VOTING_RIGHTS,
+                        APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS, SIGNIFICANT_INFLUENCE_OR_CONTROL);
+
+        var result = collectAllNatureOfControlsIntoSingleList(
+                personNatureOfControlTypes,
+                trusteesNatureOfControlTypes,
+                firmNatureOfControlTypes,
+                trustControlNatureOfControlTypes,
+                ownerOfLandPersonNatureOfControlJurisdictions,
+                ownerOfLandOtherEntityNatureOfControlJurisdictions,
+                firmControlNatureOfControlTypes,
+                true);
+
+        assertEquals(26, result.size());
+        assertEquals("OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_PERSON", result.get(0));
+        assertEquals("OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_PERSON", result.get(1));
+        assertEquals("OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_PERSON", result.get(2));
+        assertEquals("OE_SIGINFLUENCECONTROL_AS_PERSON", result.get(3));
+        assertEquals("OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_TRUST", result.get(4));
+        assertEquals("OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_TRUST", result.get(5));
+        assertEquals("OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_TRUST", result.get(6));
+        assertEquals("OE_SIGINFLUENCECONTROL_AS_TRUST", result.get(7));
+        assertEquals("OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_FIRM", result.get(8));
+        assertEquals("OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_FIRM", result.get(9));
+        assertEquals("OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_FIRM", result.get(10));
+        assertEquals("OE_SIGINFLUENCECONTROL_AS_FIRM", result.get(11));
+        assertEquals("OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_CONTROLOVERTRUST", result.get(12));
+        assertEquals("OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_CONTROLOVERTRUST", result.get(13));
+        assertEquals("OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_CONTROLOVERTRUST", result.get(14));
+        assertEquals("OE_SIGINFLUENCECONTROL_AS_CONTROLOVERTRUST", result.get(15));
+        assertEquals("OE_REGOWNER_AS_NOMINEEPERSON_ENGLANDWALES", result.get(16));
+        assertEquals("OE_REGOWNER_AS_NOMINEEPERSON_SCOTLAND", result.get(17));
+        assertEquals("OE_REGOWNER_AS_NOMINEEPERSON_NORTHERNIRELAND", result.get(18));
+        assertEquals("OE_REGOWNER_AS_NOMINEEANOTHERENTITY_ENGLANDWALES", result.get(19));
+        assertEquals("OE_REGOWNER_AS_NOMINEEANOTHERENTITY_SCOTLAND", result.get(20));
+        assertEquals("OE_REGOWNER_AS_NOMINEEANOTHERENTITY_NORTHERNIRELAND", result.get(21));
+        assertEquals("OE_OWNERSHIPOFSHARES_MORETHAN25PERCENT_AS_CONTROLOVERFIRM", result.get(22));
+        assertEquals("OE_VOTINGRIGHTS_MORETHAN25PERCENT_AS_CONTROLOVERFIRM", result.get(23));
+        assertEquals("OE_RIGHTTOAPPOINTANDREMOVEDIRECTORS_AS_CONTROLOVERFIRM", result.get(24));
+        assertEquals("OE_SIGINFLUENCECONTROL_AS_CONTROLOVERFIRM", result.get(25));
     }
 
     @Test
